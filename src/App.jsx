1980205ccc10a5213178373063591abb714ad24d
Ayushkant3011 <ayushkant17@gmail.com>
2026-08-14 13:14:12 +0530

    React router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NavBar from "./components/navbar"
 
 
@@ -5,6 +6,15 @@ function App() {
 
   return (
     <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>BasePage</div>}/>
+          <Route path="/login" element={<div>LoginPage</div>}/>
+          <Route path="/test" element={<div>TestPage</div>}/>
+        </Routes>
+
+      </BrowserRouter>
+
       <NavBar/>  
       <h1 className="text-3xl font-bold underline">DevTinder-WEB</h1>
       
