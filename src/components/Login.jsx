4a663dd3c0876d4ab11b0a8c214b68307e98bdc6
Ayushkant3011 <ayushkant17@gmail.com>
2026-08-17 16:07:53 +0530

    FeedPage added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,23 +2,26 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
     const [emailId, setEmailId] = useState("virat@gmail.com");
     const [password, setPassword] = useState("Virat@123");
     const dispatch = useDispatch();
-
+    const navigate = useNavigate()
 
     const handleLogin = async () =>{
         try{
             const response = await axios.post(
-                "http://localhost:3011/login", {
+                BASE_URL + "/login", {
                 emailId,
                 password,
                 },
                 {withCredentials: true}
             );
             dispatch(addUser(response.data));
+            navigate("/")
         }catch(err){
             console.log(err);
         }
