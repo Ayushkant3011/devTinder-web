b7e2bdba7536cda54923b84726e91c261e00944e
Ayushkant3011 <ayushkant17@gmail.com>
2026-08-20 16:35:31 +0530

    requests page ui added

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -28,17 +28,17 @@ const Connections = () => {
 
     if(!connections) return;
 
-    if(connections.length === 0) return (<h1 className='text-3xl font-extrabold '>Connections</h1>);
+    if(connections.length === 0) return (<h1 className='text-3xl font-extrabold '>No Connections Found !!</h1>);
 
   return (
     <div className='text-center my-10'>
       <h1 className='text-3xl font-extrabold '>Connections</h1>
 
       {connections.map((connection) =>{
-        const {firstName, lastName, photoUrl, age, gender, about} = connection;
+        const { _id, firstName, lastName, photoUrl, age, gender, about} = connection;
 
         return (
-            <div className='flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto'>
+            <div key={_id} className='flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto'>
                 <div>
                     <img
                         alt='Photo'
--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addRequests } from '../utils/requestSlice';
 
 const Requests = () => {
+    const requests = useSelector((store) => store.requests)
+    const dispatch = useDispatch();
     const fetchRequest = async()=>{
-        const dispatch = useDispatch()
         try{
             const res = await axios.get(
                 BASE_URL + "/user/requests/received",
@@ -23,9 +24,42 @@ const Requests = () => {
     useEffect(() =>{
         fetchRequest();
     },[]);
+
+    if(!requests) return;
+
+    if(requests.length === 0) return (<h1 className='text-3xl font-extrabold '>No Requests Found!!</h1>);
+
   return (
-    <div>
-      
+    <div className='text-center my-10'>
+      <h1 className='text-3xl font-extrabold '>Connection Requests</h1>
+
+      {requests.map((request) =>{
+        const { _id, firstName, lastName, photoUrl, age, gender, about} = request.fromUserId;
+
+        return (
+            <div key={_id} className='flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto'>
+                <div>
+                    <img
+                        alt='Photo'
+                        className='w-20 h-20 rounded-full'
+                        src={photoUrl}
+                    />
+                </div>
+                <div className='text-left mx-4'>
+                    <h2 className='font-bold text-xl'>
+                        {firstName + " " + lastName}
+                    </h2>
+                    {age && gender && <p>{age + ", " + gender}</p>}
+                    <p>{about}</p>
+                </div>
+                <div>
+                    <button className="btn btn-soft btn-primary bg-red-600 mx-2 rounded-sm">Reject</button>
+                    <button className="btn btn-soft btn-secondary bg-blue-400 mx-2 rounded-sm">Accept</button>
+                </div>
+            </div>
+        );
+      })}
+
     </div>
   )
 }
