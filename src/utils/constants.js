b729607f08eac517550d139b56184cba4e56e9c5
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-14 21:49:13 +0530

    constants upodated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL = 
-    location.hostname === "localhost" ? "http://localhost:3011" : "/api";
+export const BASE_URL =
+    location.hostname === "localhost"
+        ? "http://localhost:3011"
+        : "https://YOUR-RENDER-BACKEND.onrender.com";
