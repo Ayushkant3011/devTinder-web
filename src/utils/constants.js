3d0fd8a843c2148ff22ae1ff24fcfdd3aacc521d
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-14 23:29:07 +0530

    ? removed from backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
     location.hostname === "localhost"
         ? "http://localhost:3011"
-        : "https://devtinder-k69o.onrender.com/";
+        : "https://devtinder-k69o.onrender.com";
