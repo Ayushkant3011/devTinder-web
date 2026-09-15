e7c0e57d64d18092bfabbc6fa6fa1ea62d5c4f1c
Ayushkant3011 <ayushkant17@gmail.com>
2026-09-15 20:06:07 +0530

    Socket connection added and chat working

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { createSocketConnection } from '../utils/socket';
+import { useSelector } from 'react-redux';
 
 const Chat = () => {
     const {targetUserId} = useParams();
-    const [messages, setMessages] = useState([{ text: "Hello World" }]);
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+    const user = useSelector(store => store.user);
+    const userId = user?._id;
 
-    console.log(targetUserId);
+
+    useEffect(() =>{
+        if(!userId) return;
+        const socket = createSocketConnection();
+        socket.emit("joinChat", {
+            firstName: user.firstName,
+            userId, 
+            targetUserId,
+        });
+
+        socket.on("MessageReceived", ({ firstName, text, photoUrl}) =>{
+            console.log(firstName + " :  " + text);
+            setMessages((messages) =>[...messages, { firstName, text, photoUrl }]);
+        });
+
+        return () =>{
+            socket.disconnect();
+        }
+    }, [userId, targetUserId]);
+
+    const sendMessage = () =>{
+        const socket = createSocketConnection();
+        socket.emit("sendMessage", {
+            firstName: user.firstName, 
+            userId, 
+            targetUserId, 
+            text:newMessage,
+            photoUrl: user.photoUrl
+        });
+        setNewMessage("");
+    };
+
     return (
         <div className='w-1/2 mx-auto border-4 border-gray-600 m-5 h-[70vh] flex flex-col'>
             <h1 className="text-3xl font-extrabold border-b">
@@ -20,15 +56,15 @@ const Chat = () => {
                             <div className="w-10 rounded-full">
                             <img
                                 alt="Tailwind CSS chat bubble component"
-                                src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+                                src={msg.photoUrl}
                             />
                             </div>
                         </div>
                         <div className="chat-header">
-                            Obi-Wan Kenobi
+                            {msg.firstName}
                             <time className="text-xs opacity-50">12:45</time>
                         </div>
-                        <div className="chat-bubble bg-primary">You were the Chosen One!</div>
+                        <div className="chat-bubble bg-primary">{msg.text}</div>
                         <div className="chat-footer opacity-50">Delivered</div>
                         </div>
                     );
@@ -36,8 +72,15 @@ const Chat = () => {
             </div>
 
             <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
-                <input className='flex-1 border border-gray-500 rounded p-2'/>
-                <button className='btn btn-primary'> Send </button>
+                <input 
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)} 
+                    className='flex-1 border border-gray-500 rounded p-2'/>
+                <button
+                    onClick={sendMessage}
+                    className='btn btn-primary'> 
+                    Send 
+                </button>
             </div>
         </div>
     );
